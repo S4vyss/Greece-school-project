c3380263a974ef16232f36ad84abaeb49daf4c60
S4vyss <mlodywojownik345@gmail.com>
2022-02-22 16:30:57 +0100

    Error page

--- src/Pages/Error1.jsx
+++ src/Pages/Error1.jsx
@@ -1,7 +1,14 @@
+import '../style/stylePages/error.css'
+
 const Error1 = () => {
   return (
-    <div>
-      <h1>This specific page doesn't exist</h1>
+    <div className='errorDiv'>
+      <div className="err">
+        <h1>4</h1>
+        <h1>0</h1>
+        <h1>4</h1>
+        <h2>This specific page doesn't exist</h2>
+      </div>
     </div>
   )
 }
