c83d6e28eb40f527456863ef85450dfbeba07b6f
S4vyss <mlodywojownik345@gmail.com>
2022-02-23 12:49:12 +0100

    Bibliografia page done

--- src/Pages/Bibliografia.jsx
+++ src/Pages/Bibliografia.jsx
@@ -1,7 +1,65 @@
+import '../style/stylePages/bibliografia.css'
+
 const Bibliografia = () => {
   return (
-    <div>
-      <h1>Bibliografia</h1>
+    <div className="container-b">
+      <div className="naglowek">
+        <h1>Bibliografia</h1>
+      </div>
+      <div className="border">
+        <ul>
+          <li>
+            <a href="https://www.visitgreece.gr" target="_blank">
+              Zdjęcia i paleta kolorów strony
+            </a>
+          </li>
+          <li>
+            <a href="https://stackoverflow.com" target="_blank">
+              Pomoc naukowa do css 1
+            </a>
+          </li>
+          <li>
+            <a href="https://youtu.be/rg7Fvvl3taU" target="_blank">
+              Pomoc naukowa do css 2
+            </a>
+          </li>
+          <li>
+            <a href="https://youtu.be/VQraviuwbzU" target="_blank">
+              Pomoc naukowa do css 3
+            </a>
+          </li>
+          <li>
+            <a href="https://youtu.be/yU7jJ3NbPdA" target="_blank">
+              Pomoc naukowa do css 4
+            </a>
+          </li>
+          <li>
+            <a href="https://developer.mozilla.org/pl/" target="_blank">
+              Informacje na temat nowych znaczników html i funkcji css
+            </a>
+          </li>
+          <li>
+            <a href="https://pl.wikipedia.org/wiki/Grecja" target="_blank">
+              Informacje o Grecji
+            </a>
+          </li>
+          <li>
+            <a href="https://youtu.be/4UZrsTqkcW4" target="_blank">
+              Pomoc naukowa do react.js 1
+            </a>
+          </li>
+          <li>
+            <a href="https://youtu.be/bMknfKXIFA8" target="_blank">
+              Pomoc naukowa do react.js 2
+            </a>
+          </li>
+          <li>
+            <a href="https://youtu.be/w7ejDZ8SWv8" target="_blank">
+              Pomoc naukowa do react.js 3
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
